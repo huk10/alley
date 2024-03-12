67a5036a22ed018b5dd1c3bee3e1a16a66d0432b
Hu Kun <mrhukun@gmail.com>
2024-03-12 22:55:30 +0800

    :tada: Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+
+  collectCoverage: false,
+
+  coverageDirectory: 'coverage',
+
+  coveragePathIgnorePatterns: ['__tests__'],
+
+  coverageProvider: 'v8',
+
+  resolver: "ts-jest-resolver",
+
+  // setupFiles: ['./setup.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.(spec|test).[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+};
+
+export default config;
